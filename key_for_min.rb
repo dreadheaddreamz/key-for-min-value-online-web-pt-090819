7a57674a65eeecb0a9d17f1e64a0011296260157
James Batsell <jamesbatsell05@gmail.com>
2019-09-24 00:19:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do 
   if name_hash >= 
   
 
